cc25725b4734a306f1178f52a4fdfec3305e5ee9
Exequiel Schiavo <exelu1495@gmail.com>
2025-05-17 18:59:40 -0300

    move service worker y register

--- src/main.jsx
+++ src/main.jsx
@@ -6,7 +6,7 @@ import { Provider } from "react-redux";
 import store from "./redux/store";
 import App from "./App";
 
-import { register } from "../serviceWorkerRegistration";
+import { register } from "/serviceWorkerRegistration";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <Provider store={store}>

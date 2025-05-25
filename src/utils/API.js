cf77271ba707f7e5efc08ccf44fc54c26fe5b406
Exequiel Schiavo <exelu1495@gmail.com>
2025-05-25 14:17:40 -0300

    Eliminar espacio innecesario en la URL de la API

--- src/utils/API.js
+++ src/utils/API.js
@@ -1,4 +1,4 @@
-const Api = " https://sortisbackend.onrender.com/api/ ";
+const Api = " https://sortisbackend.onrender.com/api/";
 
 /* http://localhost:3000/api/ */
 /* https://sortisbackend.onrender.com/api/ */
